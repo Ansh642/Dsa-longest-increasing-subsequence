73bf21163c702048c5d28210e87bc392870679f4
Ansh Agarwal <122566057+Ansh642@users.noreply.github.com>
2023-02-06 23:29:54 +0530

    Create temp.cpp
    Longest increasing subsequence

---
+++ temp.cpp
@@ -0,0 +1,55 @@
+class Solution
+{
+    public:
+    
+    int solve(int n, int a[],int curr,int prev,vector<vector<int>> &dp)
+    {
+        if(curr==n)
+        return 0;
+        
+        if(dp[curr][prev+1]!=-1)
+        return dp[curr][prev+1];      //  +1 becoz prev starts from -1 to avoid error
+        
+        int include=0;         // RECURSION + MEMOIZATION
+        
+        if(prev==-1 || a[curr]>a[prev])
+          include= 1+ solve(n,a,curr+1,curr,dp);
+         
+          int exclude=solve(n,a,curr+1,prev,dp);
+         
+         dp[curr][prev+1]=max(include,exclude);
+         
+         return dp[curr][prev+1];
+    }
+    
+    int solve2(int n,int a[])
+    {
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+        
+        //int include=0;
+        for(int curr=n-1;curr>=0;curr--)
+        {
+            for(int prev=curr-1; prev>=-1; prev--)
+            {
+                int include=0;
+                if(prev==-1 || a[curr]>a[prev])      //TABULATION
+                 include= 1+ dp[curr+1][curr+1];
+         
+               int exclude=dp[curr+1][prev+1];
+         
+               dp[curr][prev+1]=max(include,exclude);
+            }
+        }
+        return dp[0][0];
+        
+    }
+    
+    int longestSubsequence(int n, int a[])
+    {
+       vector<vector<int>> dp(n,vector<int> (n+1,-1));
+       //return solve(n,a,0,-1,dp);
+       
+       return solve2(n,a);
+       
+    }
+};
